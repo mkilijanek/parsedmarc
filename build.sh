6d7ddd638f85ff03062a8359fc59336f8b1444c2
Kili <michal@kilijanek.pl>
2025-10-13 21:48:33 +0200

    tweaked building script

--- build.sh
+++ build.sh
@@ -1,1 +1,3 @@
+echo $GH_PAT | docker login ghcr.io -u mkilijanek --password-stdin
+
 sudo docker buildx build --pull --no-cache --provenance=true --sbom=true -t ghcr.io/mkilijanek/parsedmarc:8.18.6 --push .
